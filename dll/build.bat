8459e4c6334e147cf742672a1a81085217e6c57f
omicronrex <65692652+omicronrex@users.noreply.github.com>
2021-06-16 03:15:13 -0300

    re-added windows xp compat

--- dll/build.bat
+++ dll/build.bat
@@ -1,5 +1,5 @@
 call "%VS120COMNTOOLS%vsvars32.bat"
 
-cl gm82core.c /O1 /MD /GS- /nologo /link /nologo /dll /out:gm82core.dll
+cl gm82core.c /O2 /GS- /nologo /link /nologo /dll /out:gm82core.dll
 
 pause
