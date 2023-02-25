b62240e50b5bc34c8a3e2f2d2d7a824ef21983e9
omicronrex <65692652+omicronrex@users.noreply.github.com>
2023-02-25 01:31:12 -0300

    nuke alpha

--- source/gm82core.h
+++ source/gm82core.h
@@ -1,8 +1,5 @@
-#ifndef gm82core
-#define gm82core
+#pragma once
 
-#define GMREAL __declspec(dllexport) double __cdecl
-#define GMSTR __declspec(dllexport) char* __cdecl
 #define _USE_MATH_DEFINES
 #define VC_EXTRALEAN
 #define WIN32_LEAN_AND_MEAN
@@ -13,7 +10,6 @@
 #include <psapi.h>
 #include <wingdi.h>
 #include <commctrl.h>
-//#include <dwmapi.h>
 
 #pragma comment(lib, "advapi32.lib")
 #pragma comment(lib, "user32.lib")
@@ -21,13 +17,12 @@
 #pragma comment(lib, "GDI32.lib")
 #pragma comment(lib, "comctl32.lib")
 #pragma comment(lib, "ntdll.lib")
-//#pragma comment(lib, "Dwmapi.lib")
 
-#define wstr wchar_t*
+#define GMREAL __declspec(dllexport) double __cdecl
+#define GMSTR __declspec(dllexport) char* __cdecl
 
+typedef wchar_t* wstr;
 typedef unsigned long long int uint64;
 
 //force msbuild to not mangle the "secret" windows api function definition
 extern VOID WINAPI RtlGetNtVersionNumbers(LPDWORD pMajor, LPDWORD pMinor, LPDWORD pBuild);
-
-#endif
